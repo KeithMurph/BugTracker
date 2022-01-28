ae742216db4763264ba4d15434673e7df3db60a5
KeithMurph <keithmurphy94@gmail.com>
2022-01-27 23:18:25 -0800

    First Real commit!!! Lets build some components!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+
+const dummyTickets = [
+  {
+    id: "1",
+    title: "Build app",
+    priority: 'high',
+    date: new Date(2022, 1, 27),
+
+  },
+  {
+    id: "2",
+    title: "Tickets Need an Assignee",
+    priority: 'high',
+    date: new Date(2022, 1, 27),
+
+  },
+  {
+    id: "3",
+    title: "Tickets need an Admin",
+    priority: 'high',
+    date: new Date(2022, 1, 27),
+  },
+  {
+    id: "4",
+    title: "Tickets Need a Finished/ check for review ",
+    priority: 'high',
+    date: new Date(2022, 1, 27),
+  },
+  {
+    id: "5",
+    title: "Stylize Tickets",
+    priority: 'high',
+    date: new Date(2022, 1, 27),
+  },
+];
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <h2>Bug Tracker</h2>
+        
+
+        <hr/>
+        <h3>Issue 1</h3>
+        <h3>Priority: High</h3>
+        <h3>Build App!!</h3>
+        <h3>Assigned: Keith</h3>
     </div>
+
+
   );
 }
 
---
+++ src/components/Tickets/Tickets.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import './Tickets.css'
+
+// const Tickets = () => {
+//     return (
+//         <li>
+//             <div>
+//                 <h2>First Ticket</h2>
+//                 <p>Build Whole app!</p>
+//                 <p>Priority: High</p>
+//             </div>
+//         </li>
+
+//     )}
+
+// export default Tickets;
